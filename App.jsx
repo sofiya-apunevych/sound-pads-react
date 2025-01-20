5250d8adc2d0a2a60b33275e7882e90f08fcb27c
sofiya-apunevych <62404390+sofiya-apunevych@users.noreply.github.com>
2025-01-20 13:12:41 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+export default function App() {
+    const [count, setCount] = React.useState(0)
+    
+    /**
+     * Challenge: 
+     * Add functionality to the minus button
+     */
+    
+    function add() {
+        setCount(count + 1)
+    }
+    
+    function subtract() {
+        setCount(count - 1)
+    }    
+    
+    return (
+        <main className="container">
+            <h1>How many times will Bob say "state" in this section?</h1>
+            <div className="counter">
+                <button className="minus" onClick={subtract} aria-label="Decrease count">–</button>
+                <h2 className="count">{count}</h2>
+                <button className="plus" onClick={add} aria-label="Increase count">+</button>
+            </div>
+        </main>
+    )
+}
