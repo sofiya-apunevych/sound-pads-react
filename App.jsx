938c1a0b4e48779fa465cb4c5d06153413c91c0f
sofiya-apunevych <62404390+sofiya-apunevych@users.noreply.github.com>
2025-01-21 08:12:33 +0200

    Add files via upload
    Add Global State in React

--- App.jsx
+++ App.jsx
@@ -1,28 +1,30 @@
 import React from "react"
+import padsData from "./pads"
+import Pad from "./Pad"
 
 export default function App() {
-    const [count, setCount] = React.useState(0)
+    const [pads, setPads] = React.useState(padsData)
     
     /**
-     * Challenge: 
-     * Add functionality to the minus button
+     * Challenge: Create a toggle() function that logs
+     * "clicked!" to the console
+     * 
+     * Pass that function down to each of the Pad components
+     * and set it up so when they get clicked, the function runs
      */
     
-    function add() {
-        setCount(count + 1)
+    function toggle() {
+        console.log("Clicked!")
     }
     
-    function subtract() {
-        setCount(count - 1)
-    }    
+    const buttonElements = pads.map(pad => (
+        <Pad toggle={toggle} key={pad.id} color={pad.color} on={pad.on}/>
+    ))
     
     return (
-        <main className="container">
-            <h1>How many times will Bob say "state" in this section?</h1>
-            <div className="counter">
-                <button className="minus" onClick={subtract} aria-label="Decrease count">–</button>
-                <h2 className="count">{count}</h2>
-                <button className="plus" onClick={add} aria-label="Increase count">+</button>
+        <main>
+            <div className="pad-container">
+                {buttonElements}
             </div>
         </main>
     )
